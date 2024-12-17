16f4aa9c3f8a04896dae5b294a23344a0ea0f42d
Abdulwariz <bookbodmas31@gmail.com>
2024-12-17 18:52:19 +0100

    100% done with the UI, 99% done with JS

--- script.js
+++ script.js
@@ -7,11 +7,25 @@ const answers = document.querySelectorAll('.answers');
 
 
 //activate faq button
-faqBtns.forEach((btn) => {
+faqBtns.forEach((btn, index) => {
     btn.addEventListener('click', () => {
-        plusBtn.classList.toggle('hide');
-        minusBtn.classList.toggle('show');
-        answers.classList.toggle('show');
 
-    })
+        plusBtn.forEach((plus) => {
+            plus.classList.remove('hide');
+        })
+
+        minusBtn.forEach((minus) => {
+            minus.classList.remove('show');
+        })
+
+        answers.forEach((answer) => {
+            answer.classList.remove('show');
+        })
+
+        plusBtn[index].classList.toggle('hide');
+
+        minusBtn[index].classList.toggle('show');
+
+        answers[index].classList.toggle('show');
+        }) 
 })
